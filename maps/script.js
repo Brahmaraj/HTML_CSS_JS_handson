61f541b62bc16a3c06f52e8ae4450f99e100b788
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-15 21:16:55 +0530

    weak maps

---
+++ maps/script.js
@@ -0,0 +1,12 @@
+var weak_map = new WeakMap;
+var map = new Map;
+let obj1 = {
+    f_name:"Brahma",
+    l_name:"Rathod",
+    age:21
+};
+let obj2 = {
+    f_name:"Omkar",
+    l_name:"Rathod",
+    age:18
+}
