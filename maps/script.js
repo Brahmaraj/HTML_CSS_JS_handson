6ef796d3976f28b3fb5a87db4e0fef51213a70e3
Brahmaraj <brahmarajrathod@gmail.com>
2022-02-13 23:46:20 +0530

    map and weakmap

--- maps/script.js
+++ maps/script.js
@@ -1,20 +1,20 @@
-var weak_map = new WeakMap;
-var map = new Map;
-let obj1 = {
-    f_name:"Brahma",
-    l_name:"Rathod",
-    age:21
-};
-let obj2 = {
-    f_name:"Omkar",
-    l_name:"Rathod",
-    age:18
-}
-let obj3 = obj1;
-map.set(obj3,"From Objec1").set(obj2,"Unique Object");
-weak_map.set(obj3,"Copy of First Object").set(obj2,"Unique Object");
-console.log(map,weak_map);
+// var weak_map = new WeakMap;
+// var map = new Map;
+// let obj1 = {
+//     f_name:"Brahma",
+//     l_name:"Rathod",
+//     age:21
+// };
+// let obj2 = {
+//     f_name:"Omkar",
+//     l_name:"Rathod",
+//     age:18
+// }
+// let obj3 = obj1;
+// map.set(obj3,"From Objec1").set(obj2,"Unique Object");
+// weak_map.set(obj3,"Copy of First Object").set(obj2,"Unique Object");
+// console.log(map,weak_map);
 
-obj3 = null;
-console.log(map.get(obj3),weak_map.get(obj3));
-console.log(map,weak_map);
+// obj3 = null;
+// console.log(map.get(obj3),weak_map.get(obj3));
+// console.log(map,weak_map);
